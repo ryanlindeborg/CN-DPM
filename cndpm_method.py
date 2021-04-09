a2623076ef250f4475c3112d507f22c8b020e7ce
Fabrice Normandin <fabrice.normandin@gmail.com>
2021-04-09 11:44:48 -0400

    Add file for cn-dpm method
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

---
+++ cndpm_method.py
@@ -0,0 +1,11 @@
+
+from sequoia.methods import Method
+from sequoia.settings import Setting
+from sequoia.settings.passive import PassiveSetting
+
+
+class CNDPM(Method, target_setting=PassiveSetting):
+    pass
+
+
+
