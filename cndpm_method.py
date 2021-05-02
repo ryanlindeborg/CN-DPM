03a2fe32c1b7fc126d63d8181c9e7a9be4e12593
ryanlindeborg <lindeborgryan@gmail.com>
2021-05-02 07:13:46 -0700

    Progress on cndpm model initialization

--- cndpm_method.py
+++ cndpm_method.py
@@ -1,21 +1,29 @@
 import gym
+import yaml
 from typing import Tuple
+from typing import ClassVar, Type
+
 from sequoia.methods import Method
 from sequoia.settings import Setting, Environment
 from sequoia.settings.passive import PassiveSetting, PassiveEnvironment, ClassIncrementalSetting
 
-# from models.ndpm_model import NdpmModel
+from models.ndpm_model import NdpmModel
 
+CNDPM_YAML_PATH = './sequoia/methods/cn_dpm/configs/cndpm.yaml'
 
 class CNDPM(Method, target_setting=ClassIncrementalSetting):
     """ A Neural Dirichlet Process Mixture Model for Task-Free Continual Learning
     
     https://arxiv.org/abs/2001.00689
-    test submodule
     """
-    
+
+    ModelType: ClassVar[Type[NdpmModel]] = NdpmModel
+
     def __init__(self, learning_rate: float = 3e-4):
         self.learning_rate = learning_rate
+
+        # We will create this when `configure` is called, before training.
+        self.model: NdpmModel
     
     def configure(self, setting: ClassIncrementalSetting):
         """Configures this method before it gets applied on the given Setting.
@@ -32,7 +40,20 @@ class CNDPM(Method, target_setting=ClassIncrementalSetting):
         print(f"Number of tasks: {number_of_tasks}")
         # TODO: Create the Model, using the image shape, the number of tasks (if that
         # makes sense), etc.
-        pass
+        # Create config from setting variable, to pass into model when initialize
+        config = yaml.load(open(CNDPM_YAML_PATH), Loader=yaml.FullLoader)
+        # TODO Set dimensions of input and output: x_c: 1
+        # x_h: 28
+        # x_w: 28
+        # y_c: 10
+        # TODO: Maybe delete and set all properties of model manually
+        self.model = self.ModelType(
+            #observation_space=setting.observation_space,
+            #action_space=setting.action_space,
+            #reward_space=setting.reward_space,
+            config,
+            writer: SummaryWriter
+        )
 
     def fit(self, train_env: Environment, valid_env: Environment):
         """Called by the Setting to give the method data to train with.
